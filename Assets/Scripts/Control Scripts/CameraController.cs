70b3c91970cb2245e97fb3bcb56f58ae04007516
KevX36 <kevincard2007@gmail.com>
2026-03-02 21:13:31 -0400

    set up camera script

---
+++ Assets/Scripts/Control Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject cam;
+    private float pitch = 0;
+    private float yaw = 0;
+    public float pitchMax = 60;
+    public float pitchMin = -60;
+    public float turnspeed = 2;
+    void Update()
+    {
+        pitch += Input.GetAxis("Mouse Y") * turnspeed;
+        if (pitch < pitchMin)
+        {
+            pitch = pitchMin;
+        }
+        if (pitch > pitchMax)
+        {
+            pitch = pitchMax;
+        }
+
+        yaw += Input.GetAxis("Mouse X") * turnspeed;
+
+        cam.transform.rotation = Quaternion.Euler(-pitch, yaw, 0);
+        transform.rotation = Quaternion.Euler(0, yaw, 0);
+    }
+}
